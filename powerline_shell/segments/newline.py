c8dc39f375e06a0575357812da306d3bd25c8465
Stuart McLean <stuart.mclean@liquidm.com>
2018-10-22 16:09:03 +0200

    customizations for segments and themes

--- powerline_shell/segments/newline.py
+++ powerline_shell/segments/newline.py
@@ -6,7 +6,7 @@ class Segment(BasicSegment):
         if self.powerline.args.shell == "tcsh":
             warn("newline segment not supported for tcsh (yet?)")
             return
-        self.powerline.append("\n",
+        self.powerline.append("\n$",
                               self.powerline.theme.RESET,
                               self.powerline.theme.RESET,
                               separator="")
